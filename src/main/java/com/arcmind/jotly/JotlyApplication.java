0b1a80b71a158b0ae301ce8f83ca017c6fa2ae5c
Prajith <prajithr004@gmail.com>
2025-07-17 21:40:37 +0530

    add files

---
+++ src/main/java/com/arcmind/jotly/JotlyApplication.java
@@ -0,0 +1,13 @@
+package com.arcmind.jotly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JotlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcmind/jotly/JotlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcmind.jotly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
